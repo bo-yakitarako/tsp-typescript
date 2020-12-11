85d27c8487a86e1acce5f99da1ec32fe68d6c973
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2020-12-11 23:28:00 +0900

    Individualできた

---
+++ src/Individual.ts
@@ -0,0 +1,80 @@
+const MUTATION_RATE = 0.03;
+
+export default class Individual {
+  private fieldChromosome: number[];
+  private fieldScore: number;
+  public static nodes: [number, number][];
+
+  constructor(...parents: [Individual, Individual, number] | []) {
+    this.fieldChromosome = [];
+    if (parents.length === 0) {
+      this.setRandomGenes();
+      this.fieldScore = this.calcScore();
+    } else {
+      const [mother, father, division] = parents;
+      this.orderCrossover(mother, father, division);
+      if (Math.random() < MUTATION_RATE) {
+        this.spontaneousMutation();
+      }
+      this.fieldScore = this.calcScore();
+    }
+  }
+
+  public get chromosome(): number[] {
+    return [...this.fieldChromosome];
+  }
+
+  public get score(): number {
+    return this.fieldScore;
+  }
+
+  private setRandomGenes(): void {
+    let genes = Individual.nodes.map((val, index) => index);
+    while (genes.length > 0) {
+      const r = Math.floor(genes.length * Math.random());
+      this.fieldChromosome.push(genes[r]);
+      genes = [...genes.slice(0, r), ...genes.slice(r + 1, genes.length)];
+    }
+  }
+
+  private orderCrossover(
+    mother: Individual,
+    father: Individual,
+    division: number
+  ): void {
+    const former = mother.chromosome.slice(0, division);
+    const latter = father.chromosome.reduce((genes, gene) => {
+      if (former.includes(gene)) {
+        return [...genes];
+      }
+      return [...genes, gene];
+    }, [] as number[]);
+    this.fieldChromosome = [...former, ...latter];
+  }
+
+  private spontaneousMutation(): void {
+    const index1 = Math.floor(this.fieldChromosome.length * Math.random());
+    let index2 = index1;
+    while (index2 === index1) {
+      index2 = Math.floor(this.fieldChromosome.length * Math.random());
+    }
+    const tmp = this.fieldChromosome[index1];
+    this.fieldChromosome[index1] = this.fieldChromosome[index2];
+    this.fieldChromosome[index2] = tmp;
+  }
+
+  private calcScore(): number {
+    return this.fieldChromosome.reduce((score, curGene, index, genes) => {
+      const nextGene = index < genes.length - 1 ? genes[index + 1] : genes[0];
+      return score + Individual.calcDistance(curGene, nextGene);
+    }, 0);
+  }
+
+  private static calcDistance(gene1: number, gene2: number) {
+    const [x1, y1] = Individual.nodes[gene1];
+    const [x2, y2] = Individual.nodes[gene2];
+    const x = x1 - x2;
+    const y = y1 - y2;
+    return Math.sqrt(x * x + y * y);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log('Hello, TSP!');
+import Individual from './Individual';
+
+const nodes: [number, number][] = [
+  [23, 39],
+  [8, 44],
+  [34, 36],
+  [12, 30],
+  [42, 37],
+  [6, 35],
+  [1, 15],
+  // [12, 25],
+  // [4, 39],
+  // [13, 42],
+  // [23, 13],
+  // [7, 39],
+  // [11, 5],
+  // [6, 44],
+  // [28, 45],
+  // [10, 7],
+  // [3, 16],
+  // [4, 19],
+  // [3, 39],
+  // [0, 2],
+  // [19, 21],
+  // [5, 43],
+  // [8, 34],
+  // [20, 39],
+  // [2, 50],
+  // [20, 26],
+  // [16, 36],
+  // [24, 30],
+  // [9, 40],
+  // [5, 22],
+  // [30, 35],
+  // [2, 0],
+  // [21, 36],
+  // [22, 28],
+  // [3, 33],
+  // [11, 36],
+  // [14, 34],
+];
+// 202.37
+Individual.nodes = nodes;
+const parent1 = new Individual();
+const parent2 = new Individual();
+const child = new Individual(parent1, parent2, 3);
+console.log(parent1.chromosome);
+console.log(parent2.chromosome);
+console.log(child.chromosome);
